5fcf308e2d66c4eb10a2ae50909f7c5ba981198c
Willy <willy.du@efrei.net>
2025-06-11 11:51:56 +0200

    Add subtract function

--- index.test.js
+++ index.test.js
@@ -1,5 +1,9 @@
-const sum = require('./index');
+const { sum, subtract } = require('./index');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('subtracts 5 - 3 to equal 2', () => {
+  expect(subtract(5, 3)).toBe(2);
 });
