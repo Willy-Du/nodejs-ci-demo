892923903d8d30964ca9bc931cf9a1a09b567da1
Willy <willy.du@efrei.net>
2025-06-11 11:55:42 +0200

    Add multiply function and test

--- index.test.js
+++ index.test.js
@@ -1,5 +1,13 @@
-const sum = require('./index');
+const { sum, subtract, multiply } = require('./index');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('subtracts 5 - 3 to equal 2', () => {
+  expect(subtract(5, 3)).toBe(2);
+});
+
+test('multiplies 3 * 4 to equal 12', () => {
+  expect(multiply(3, 4)).toBe(12);
 });
